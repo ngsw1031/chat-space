c132b71aad2a71a6d001b652cede5154dd5c13b8
ngsw1031 <nagasawa12051031@gmail.com>
2020-06-02 19:38:35 +0900

    chat-space_Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+          `<div class="main-chat__message__block">
+            <div class="main-chat__message__block__name">
+              ${message.user_name}
+            </div>
+            <div class="main-chat__message__block__name__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="main-chat__message__block__list">
+            <p class="main-chat__message__block__list__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+          `<div class="umain-chat__message__block">
+            <div class=".main-chat__message__block__name">
+              ${message.user_name}
+            </div>
+            <div class="main-chat__message__block__name__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class=".main-chat__message__block__list">
+            <p class="main-chat__message__block__list__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.main-chat__message').append(html);
+      $('.main-chat__message').animate({ scrollTop: $('.main-chat__message')[0].scrollHeight});
+      $('.main-chat__form__btn').prop('disabled', false);
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('.main-chat__form__btn').prop('disabled', false);
+  });
+  })
+});
